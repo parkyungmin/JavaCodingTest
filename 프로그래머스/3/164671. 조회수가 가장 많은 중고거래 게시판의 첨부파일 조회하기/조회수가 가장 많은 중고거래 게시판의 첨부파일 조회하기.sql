07310b03755761cd8fd2e47a12d32dd53e77513f
박경민 <118189597+parkyungmin@users.noreply.github.com>
2024-10-23 10:11:43 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,3 +1,5 @@
+/*정답 풀이 1번*/
+/*
 SELECT '/home/grep/src/' || A.BOARD_ID || '/' || FILE_ID || FILE_NAME || FILE_EXT AS FILE_PATH
   FROM USED_GOODS_BOARD A
      , USED_GOODS_FILE B
@@ -7,3 +9,20 @@ SELECT '/home/grep/src/' || A.BOARD_ID || '/' || FILE_ID || FILE_NAME || FILE_EX
                  )
  ORDER
     BY B.FILE_ID DESC
+*/
+
+/*정답 풀이 2번 - RANK함수 사용*/
+SELECT '/home/grep/src/' || A.BOARD_ID || '/' || B.FILE_ID || B.FILE_NAME || B.FILE_EXT AS FILE_PATH
+  FROM (SELECT A.BOARD_ID
+             , A.VIEWS
+             , DENSE_RANK() OVER(ORDER BY A.VIEWS DESC) AS RANK
+          FROM USED_GOODS_BOARD A
+       ) A
+     , USED_GOODS_FILE B
+ WHERE 1=1
+   AND A.BOARD_ID = B.BOARD_ID
+   AND A.RANK = 1
+ ORDER
+    BY B.FILE_ID DESC
+
+ 
