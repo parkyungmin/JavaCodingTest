9f14864200406be073bfe82eb32bf9e4291d3a86
박경민 <118189597+parkyungmin@users.noreply.github.com>
2025-04-20 22:06:48 +0900

    [level 0] Title: 특수문자 출력하기, Time: 145.90 ms, Memory: 68.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // \(이스케이프) 문자 사용하여 특수문자 출력
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+    }
+}
