f1adfc7965f2ff8fae1b99a140a97d905bf9def4
박경민 <118189597+parkyungmin@users.noreply.github.com>
2025-04-22 22:23:19 +0900

    [level 2] Title: 피보나치 수, Time: 3.33 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -5,7 +5,7 @@ class Solution {
         int sum = 0;
 
         for (int i=2; i<=n; i++) {
-          sum = (f0 + f1) % 1234567;
+          sum = (f0 + f1) % 1234567; //(a+b)(mod m)=(a(mod m)+b(mod m))(mod m)
           f0 = f1;
           f1 = sum;
         }
